c9adf01568c623687de1f08b2a7991ac51fb6abe
cjchalmers <colin.chalmers@simplex-it.nl>
2023-01-12 15:57:44 +0100

    Follow Naming Convention

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_instance" "my_example" {
     Name = var.instance_name
   }
 }
+
+resource "aws_instance" "instance_1" {
+  ami           = var.instance_1_ami
+  instance_type = var.instance_1_type
+  tags = {
+    Name = var.instance_1_name
+  }
+}
